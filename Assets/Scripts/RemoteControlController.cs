1602abdf5bda3e82ab807c67aeb6dedf883d79f4
Jing Yang <yangjing1992@gmail.com>
2017-01-18 13:56:44 -0500

    object rotation
    Added cube rotation with left and right arrows

--- Assets/Scripts/RemoteControlController.cs
+++ Assets/Scripts/RemoteControlController.cs
@@ -10,9 +10,10 @@ public class RemoteControlController : MonoBehaviour
 	private GameObject selectedCube;
 	private GameObject templateShape;
 
-
+	private float rotationOffset;
 	void Start() {
 		selectedCube = player.GetComponent<PlayerController>().selectedCube;
+		rotationOffset = 90.0f;
 	}
 
 	void FixedUpdate()
@@ -22,15 +23,25 @@ public class RemoteControlController : MonoBehaviour
 
 		templateShape = player.GetComponent<PlayerController>().templateShape;
 
-		if(Input.GetKeyDown(KeyCode.P)) {
+		if (Input.GetMouseButtonDown(0)) {
 			// create a new cube when press "P" 
-			Vector3 coord = new Vector3((int)player.transform.position[0],((int)player.transform.position[1])+0.5f, (int)player.transform.position[2]);
-			GameObject newObj = Instantiate (templateShape, coord, new Quaternion(0,0,0,0));
+			Vector3 coord = new Vector3 ((int)player.transform.position [0], ((int)player.transform.position [1]) + 0.5f, (int)player.transform.position [2]);
+			GameObject newObj = Instantiate (templateShape, coord, new Quaternion (0, 0, 0, 0));
 			print (newObj.tag);
 			newObj.tag = "Cube";
-		} else if( Input.GetKeyDown(KeyCode.O) && selectedCube.CompareTag("Cube") ){
+			newObj.transform.localScale = templateShape.transform.lossyScale;
+
+		} else if (Input.GetMouseButtonDown(1) && selectedCube.CompareTag ("Cube")) {
 			// delete the cube when press "O" 
 			selectedCube.SetActive (false);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow) && selectedCube.CompareTag ("Cube")) {
+			Vector3 rot = selectedCube.transform.localRotation.eulerAngles;
+			selectedCube.transform.rotation=Quaternion.Euler(rot.x,rot.y-rotationOffset,0);
+
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow) && selectedCube.CompareTag ("Cube")) {
+			Vector3 rot = selectedCube.transform.localRotation.eulerAngles;
+			selectedCube.transform.rotation=Quaternion.Euler(rot.x,rot.y+rotationOffset,0);
+		
 		}
 
 	}
