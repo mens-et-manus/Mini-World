d382547a4a0cad2226f52c4b60a2709248ef9989
Mark Vrablic <markvrablic@yahoo.com>
2017-01-19 14:23:55 -0600

    rotation with kinect
    added object rotation with kinect by putting right hand to the left or
    right

--- Assets/Scripts/KinectPlayerController.cs
+++ Assets/Scripts/KinectPlayerController.cs
@@ -6,7 +6,7 @@ public class KinectPlayerController : MonoBehaviour
 {
 	private FollowJoint followJoint; // Kinect Script
 	public GameObject selectedCube;
-	public GameObject templateShape;
+	public GameObject selectedBucket;
 
     void Start() {
 		// get Kinect scripts
@@ -15,19 +15,25 @@ public class KinectPlayerController : MonoBehaviour
 
 	void FixedUpdate() {
 		// read position from Right hand
-        transform.position = followJoint.ReadPosition;
+        //transform.position = followJoint.ReadPosition;
+        if (followJoint.GeneratedPosition[1] < 0.5){
+            transform.position = new Vector3 (followJoint.GeneratedPosition[0],0.5f,followJoint.GeneratedPosition[2]);
+        }
+        else { transform.position = followJoint.GeneratedPosition; }
     }
 
     void OnTriggerEnter(Collider other) {
 
-		selectedCube = other.gameObject;
+		//selectedCube = other.gameObject;
         if (other.gameObject.CompareTag("Bucket")){
-			// collect the material and shape
-			GetComponent<Renderer>().material = selectedCube.GetComponent<Renderer>().material;
-			templateShape = selectedCube;
+            // collect the material and shape
+            selectedBucket = other.gameObject;
+			GetComponent<Renderer>().material = selectedBucket.GetComponent<Renderer>().material;
+			
         }
         else if (other.gameObject.CompareTag("Cube")){
-			// assign the material to a cube
+            // assign the material to a cube
+            selectedCube = other.gameObject;
             other.gameObject.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
         }
 
--- Assets/Scripts/KinectRemoteControlController.cs
+++ Assets/Scripts/KinectRemoteControlController.cs
@@ -7,12 +7,14 @@ public class KinectRemoteControlController : MonoBehaviour
 	public GameObject cube;
 	public float thresholdTop;
 	public float thresholdBottom;
+    public float thresholdLeft;
+    public float thresholdRight;
 	public GameObject player;
 
 	private GameObject selectedCube;
 
 	private FollowJoint followJoint; // Kinect Script
-	private GameObject templateShape;
+	private GameObject selectedBucket;
 
 	void Start() {
 		// get Kinect scripts
@@ -23,22 +25,31 @@ public class KinectRemoteControlController : MonoBehaviour
 	void FixedUpdate(){
 
 		selectedCube = player.GetComponent<KinectPlayerController>().selectedCube;
-		templateShape = player.GetComponent<PlayerController>().templateShape;
+		selectedBucket = player.GetComponent<KinectPlayerController>().selectedBucket;
 		// read position from Right hand
 		transform.position = followJoint.ReadPosition;
 
 		if (transform.position[1] >= thresholdTop && !selectedCube){
 			// Left hand is high -> create a new cube
-			Vector3 coord = new Vector3((int)player.transform.position[0], 0, (int)player.transform.position[2]);
-			GameObject newCube = Instantiate(templateShape, coord, new Quaternion(0, 0, 0, 0));
+			Vector3 coord = new Vector3((int)player.transform.position[0], (int)player.transform.position[1], (int)player.transform.position[2]);
+			GameObject newCube = Instantiate(selectedBucket, coord, new Quaternion(0, 0, 0, 0));
 			newCube.tag = "Cube";
-			newCube.transform.localScale = templateShape.transform.lossyScale * 3;
+			newCube.transform.localScale = selectedBucket.transform.lossyScale * 3;
+            //player.selectedCube = null;
 		} else if( transform.position[1] <= thresholdBottom && selectedCube.gameObject.CompareTag("Cube") ){
 			// Left hand is low -> delete the cube
 			player.GetComponent<KinectPlayerController>().selectedCube = null; 
 			selectedCube.gameObject.SetActive (false);
-		}
+		} else if( transform.position[0]<= thresholdLeft && selectedCube.gameObject.CompareTag("Cube")){
+            //rotate left
+            Vector3 rot = selectedCube.transform.localRotation.eulerAngles;
+            selectedCube.transform.rotation = Quaternion.Euler(rot.x, rot.y - 1.0f, 0);
+        } else if (transform.position[0] >= thresholdRight && selectedCube.gameObject.CompareTag("Cube")) {
+            //rotate right
+            Vector3 rot = selectedCube.transform.localRotation.eulerAngles;
+            selectedCube.transform.rotation = Quaternion.Euler(rot.x, rot.y + 1.0f, 0);
+        }
 
-	}
+    }
 
 }
