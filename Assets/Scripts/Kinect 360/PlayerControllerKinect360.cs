7a43a7e52b318dca21a421215bcd35801191e1c2
Cattalyya <cattalyya@hotmail.com>
2017-01-19 10:45:17 -0800

    Implement PlayerController and RemoteCtrl for Kinect360

---
+++ Assets/Scripts/Kinect 360/PlayerControllerKinect360.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerControllerKinect360 : MonoBehaviour
+{
+    public GameObject selectedCube;
+    public KinectWrapper.NuiSkeletonPositionIndex TrackedJoint = KinectWrapper.NuiSkeletonPositionIndex.HandRight;
+    
+    void Start() {
+    }
+
+
+    void FixedUpdate() {
+        KinectManager manager = KinectManager.Instance;
+        int iJointIndex = (int)TrackedJoint;
+
+        if (manager.IsUserDetected()) {
+
+            uint userId = manager.GetPlayer1ID();
+
+            if (manager.IsJointTracked(userId, iJointIndex)) {
+                Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, iJointIndex);
+
+                if (posJoint != Vector3.zero) {
+                    
+                    transform.position = posJoint*10;
+                    //print(transform.position);
+                    transform.position = new Vector3(transform.position[0], transform.position[1], transform.position[2]-15);
+                }
+            }
+        }
+    
+    }
+
+    void OnTriggerEnter(Collider other) {
+
+        selectedCube = other.gameObject;
+        if (other.gameObject.CompareTag("Bucket")) {
+            // collect the material
+            GetComponent<Renderer>().material = other.gameObject.GetComponent<Renderer>().material;
+        }
+        else if (other.gameObject.CompareTag("Cube")) {
+            // assign the material to a cube
+            other.gameObject.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other) {
+        selectedCube = null;
+    }
+}
---
+++ Assets/Scripts/Kinect 360/RemoteControlControllerKinect360.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RemoteControlControllerKinect360 : MonoBehaviour
+{
+    public GameObject cube;
+    public float thresholdTop;
+    public float thresholdBottom;
+    public GameObject player;
+
+    private GameObject selectedCube;
+    
+    public KinectWrapper.NuiSkeletonPositionIndex TrackedJoint = KinectWrapper.NuiSkeletonPositionIndex.HandLeft;
+
+
+    void Start() {
+        selectedCube = player.GetComponent<PlayerControllerKinect>().selectedCube;
+    }
+
+    void FixedUpdate()
+    {
+
+        selectedCube = player.GetComponent<PlayerControllerKinect>().selectedCube;
+
+        KinectManager manager = KinectManager.Instance;
+        int iJointIndex = (int)TrackedJoint;
+
+        if (manager.IsUserDetected())
+        {
+
+            uint userId = manager.GetPlayer1ID();
+
+            if (manager.IsJointTracked(userId, iJointIndex))
+            {
+                Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, iJointIndex);
+
+                if (posJoint != Vector3.zero)
+                {
+                    // read position from Right hand
+                    transform.position = posJoint * 10;
+                    transform.position = new Vector3(transform.position[0], transform.position[1], transform.position[2] - 15);
+
+                    print(selectedCube);
+                    if (transform.position[1] >= thresholdTop && !selectedCube)
+                    {
+                        //print("CREATE");
+                        // Left hand is high -> create a new cube
+                        Vector3 coord = new Vector3((int)player.transform.position[0], ((int)player.transform.position[1]) + 0.5f, (int)player.transform.position[2]);
+                        GameObject newCube = Instantiate(cube, coord, new Quaternion(0, 0, 0, 0));
+                        newCube.GetComponent<Renderer>().material = player.GetComponent<Renderer>().material;
+                    }
+                    else if (transform.position[1] <= thresholdBottom )
+                    {
+                        //if(selectedCube.gameObject.CompareTag("Cube"))
+                        print("DELETE");
+                        print(selectedCube);
+                        
+
+                        // Left hand is low -> delete the cube
+                        player.GetComponent<PlayerControllerKinect>().selectedCube = null;
+                        selectedCube.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+}
